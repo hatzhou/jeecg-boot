0fae056f82ece94e278f0de58897f63e2f0902b4
hatzhou <hatzhou@163.com>
2021-12-29 10:23:58 +0800

    前端组件

---
+++ ant-design-vue-jeecg/src/views/rany/modules/Tb02InstructionsModal.vue
@@ -0,0 +1,60 @@
+<template>
+  <j-modal
+    :title="title"
+    :width="width"
+    :visible="visible"
+    switchFullscreen
+    @ok="handleOk"
+    :okButtonProps="{ class:{'jee-hidden': disableSubmit} }"
+    @cancel="handleCancel"
+    cancelText="关闭">
+    <tb02-instructions-form ref="realForm" @ok="submitCallback" :disabled="disableSubmit"></tb02-instructions-form>
+  </j-modal>
+</template>
+
+<script>
+
+  import Tb02InstructionsForm from './Tb02InstructionsForm'
+  export default {
+    name: 'Tb02InstructionsModal',
+    components: {
+      Tb02InstructionsForm
+    },
+    data () {
+      return {
+        title:'',
+        width:800,
+        visible: false,
+        disableSubmit: false
+      }
+    },
+    methods: {
+      add () {
+        this.visible=true
+        this.$nextTick(()=>{
+          this.$refs.realForm.add();
+        })
+      },
+      edit (record) {
+        this.visible=true
+        this.$nextTick(()=>{
+          this.$refs.realForm.edit(record);
+        })
+      },
+      close () {
+        this.$emit('close');
+        this.visible = false;
+      },
+      handleOk () {
+        this.$refs.realForm.submitForm();
+      },
+      submitCallback(){
+        this.$emit('ok');
+        this.visible = false;
+      },
+      handleCancel () {
+        this.close()
+      }
+    }
+  }
+</script>
---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/rany/mapper/Tb02InstructionsMapper.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.rany.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.jeecg.modules.rany.entity.Tb02Instructions;
+
+/**
+ * @Description: tb02_instructions
+ * @Author: jeecg-boot
+ * @Date:   2021-11-23
+ * @Version: V1.0
+ */
+public interface Tb02InstructionsMapper extends BaseMapper<Tb02Instructions> {
+
+}
